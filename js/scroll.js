80761f2577c6ca43380a91584470b0b0158ed6dc
Hansen Emanuel <hansenemanuel@hotmail.com>
2021-09-23 00:53:47 +0700

    add form submission space

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,26 @@
+window.addEventListener('scroll', reveal);
 window.addEventListener('scroll', revealup);
 window.addEventListener('scroll', revealdown);
 window.addEventListener('scroll', revealleft);
 window.addEventListener('scroll', revealright);
 window.addEventListener('scroll', revealzoom);
 
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i<reveals.length; i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if (revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        // else{
+        //     reveals[i].classList.remove('active');
+        // }
+    }
+}
 function revealup(){
     var reveals = document.querySelectorAll('.revealup');
 
